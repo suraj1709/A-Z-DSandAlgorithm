d5a45f05d6b65e978ab68342d461d16781d9fb58
suraj <surajdey8603@gmail.com>
2021-06-03 21:52:57 +0530

    Array QUESTIONS

--- src/com/ds/algo/array/RemoveDuplicate.java
+++ src/com/ds/algo/array/RemoveDuplicate.java
@@ -6,24 +6,40 @@ package com.ds.algo.array;
 * Example :
 * Input : [1,2,2,3,3]
 * Output : [1,2,3]
-* */
+*
+*/
+
+
+/*
+ *
+ * Time Complexity :O(n)
+ * Space Complexity : O(1)
+ *
+ * */
 public class RemoveDuplicate {
 
-    public static int removeDuplicate(int[] arr) {
+    public static int[] removeDuplicate(int[] arr) {
         int n = arr.length;
         int count = n > 0 ? 1 : 0;
         int j = 0;
         for (int i = 0; i < n; i++) {
-            if(arr[i] == arr[j]) {
-                arr[j] = arr[i];
-            } else {
+            if(arr[i] != arr[j]) {
                 j++;
                 count++;
                 arr[j] = arr[i];
             }
+
         }
-        return count;
+        return arr;
     }
 
+    public static void main(String[] args) {
+        int[] response=RemoveDuplicate.removeDuplicate(new int[]{1,2,2,3,3});
+        for (int ans: response)
+        {
+            System.out.print(ans+ "  ");
+
+        }
+    }
 
 }
